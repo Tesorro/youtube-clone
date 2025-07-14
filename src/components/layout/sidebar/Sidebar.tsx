7e706c685f0215d8911d5584e9d7dc3d31197242
Zaur <z.tsoroev@original-group.ru>
2025-07-14 19:35:50 +0300

    added colorise to active page

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { SidebarHeader } from './header/SidebarHeader'
-import { SidebarMenu } from './menus/sidebar-main-menu/SidebarMenu'
+import { SidebarMenu } from './menus/sidebar-menu/SidebarMenu'
 import { SubscriptionsMenu } from './menus/subscriptions-menu/SubscriptionsMenu'
 import { MORE_SIDEBAR_DATA, SIDEBAR_DATA } from './sidebar.data'
 
